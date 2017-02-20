433b4bdc8997d82d2d1c64a63114de39ac438339
Kiharu Sasaki <kiharu.sasaki@gmail.com>
2017-02-20 21:09:24 +0900

    メールアドレスによるユーザ登録時のバグを修正(未完成)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u|
-      u.permit(:user_id, :password, :password_confirmation)
+      u.permit(:user_id, :email, :password, :password_confirmation)
     }
     devise_parameter_sanitizer.permit(:account_update) { |u|
-      u.permit(:user_id, :password, :remember_me)
+      u.permit(:user_id, :email, :password, :remember_me)
     }
     devise_parameter_sanitizer.permit(:account_update) { |u|
       u.permit(:name, :email, :phone_number, :description)
